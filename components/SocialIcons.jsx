83d524f09e26ac89507242e8a5f2a3028ec0743e
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-20 21:23:28 +0530

    Create another component for Social Icons

---
+++ components/SocialIcons.jsx
@@ -0,0 +1,35 @@
+import {FaGithub, FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn} from 'react-icons/fa'
+
+const SocialIcons = ({gi, fb, tw, is, li}) => {
+    return(<>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            {
+                gi ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={gi}>
+                        <FaGithub />
+                </a> : null
+            }
+            {
+                li ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={li}>
+                        <FaLinkedinIn />
+                </a> : null
+            }
+            {
+                tw ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={tw}>
+                        <FaTwitter />
+                </a> : null
+            }
+            {
+                fb ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={fb}>
+                        <FaFacebookF />
+                </a> : null
+            }
+            {
+                is ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={is}>
+                        <FaInstagram />
+                </a> : null
+            }
+        </span>
+    </>)
+}
+
+export default SocialIcons
