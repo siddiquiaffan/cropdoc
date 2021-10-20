6ff007c5d9b17b4dc12aff4e2fa71ec76cd9d6aa
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-20 21:29:14 +0530

    Change card design

--- components/SocialIcons.jsx
+++ components/SocialIcons.jsx
@@ -2,29 +2,29 @@ import {FaGithub, FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn} from 'react
 
 const SocialIcons = ({gi, fb, tw, is, li}) => {
     return(<>
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start text-xl">
             {
-                gi ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={gi}>
+                gi ? <a className="text-gray-100 px-3" target='_blank' rel="noreferrer" href={gi}>
                         <FaGithub />
                 </a> : null
             }
             {
-                li ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={li}>
+                li ? <a className="text-gray-100 px-3" target='_blank' rel="noreferrer" href={li}>
                         <FaLinkedinIn />
                 </a> : null
             }
             {
-                tw ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={tw}>
+                tw ? <a className="text-gray-100 px-3" target='_blank' rel="noreferrer" href={tw}>
                         <FaTwitter />
                 </a> : null
             }
             {
-                fb ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={fb}>
+                fb ? <a className="text-gray-100 px-3" target='_blank' rel="noreferrer" href={fb}>
                         <FaFacebookF />
                 </a> : null
             }
             {
-                is ? <a className="text-gray-100 px-3 text-xl" target='_blank' rel="noreferrer" href={is}>
+                is ? <a className="text-gray-100 px-3" target='_blank' rel="noreferrer" href={is}>
                         <FaInstagram />
                 </a> : null
             }
