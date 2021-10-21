ead878f858a0a4249e0206f5e8962339e5a81fd3
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-21 12:11:41 +0530

    Add env

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images:{
     domains: ['telegra.ph']
+  },
+  env: {
+    MEMBERS: JSON.parse(process.env.MEMBERS) || [], // Array of team members [Must be in JSON format]
   }
 }
